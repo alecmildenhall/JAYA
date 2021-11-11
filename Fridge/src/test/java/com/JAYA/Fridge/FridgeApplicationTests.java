e17544d62097b90106b11bffdd8d3d0ed9556604
Amber Mildenhall <60529049+ambermildenhall@users.noreply.github.com>
2021-11-10 21:00:33 -0500

    Hello World
    Basic "Hello World" functionality for building off of. "Hello World" is returned at the "/" endpoint. The project is initialized with the Spring Web, Spring Data JPA, and PostgreSQL Driver dependencies, although the PostgreSQL Driver dependency is currently commented out in the pom.xml due to no database being connected.

---
+++ Fridge/src/test/java/com/JAYA/Fridge/FridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JAYA.Fridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
