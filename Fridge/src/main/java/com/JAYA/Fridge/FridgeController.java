3bc7e76af7b58829f98d84bde3946ee196d688d8
jade-i-k <55996281+jade-i-k@users.noreply.github.com>
2021-11-12 13:46:58 -0500

    Added controller and service files

--- Fridge/src/main/java/com/JAYA/Fridge/FridgeController.java
+++ Fridge/src/main/java/com/JAYA/Fridge/FridgeController.java
@@ -1,5 +1,24 @@
 package com.JAYA.Fridge;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
+@RestController
+@RequestMapping(path = "api/v1/fridge")
 public class FridgeController {
+
+    private final FridgeService fridgeService;
+
+    @Autowired
+    public FridgeController(FridgeService fridgeService){
+        this.fridgeService = fridgeService;
+    }
+
+    @GetMapping
+	public List<Fridge> getFridge(){
+        return fridgeService.getFridge();
+	}
     
 }
--- Fridge/src/main/java/com/JAYA/Fridge/FridgeService.java
+++ Fridge/src/main/java/com/JAYA/Fridge/FridgeService.java
@@ -1,5 +1,14 @@
 package com.JAYA.Fridge;
+import org.springframework.stereotype.Service;
 
+import java.time.*;
+import java.util.*;
+
+
+@Service
 public class FridgeService {
-    
+    public List<Fridge> getFridge(){
+        return List.of(new Fridge(1L, "Pumpkin", 35L, 30L));
+    }
+    
 }
