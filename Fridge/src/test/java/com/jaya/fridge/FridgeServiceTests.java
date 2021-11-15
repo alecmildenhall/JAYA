a4b991cd0caed8c2c57d625247693b01cc93d244
Yael Kelmer <yrk2107@barnard.edu>
2021-11-15 17:34:17 -0500

    added some unit tests for updateFood() method, changed pom.xml file to include h2database

---
+++ Fridge/src/test/java/com/jaya/fridge/FridgeServiceTests.java
@@ -0,0 +1,57 @@
+package com.jaya.fridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+@DataJpaTest
+public class FridgeServiceTests {
+  @Autowired
+  private TestEntityManager entityManager;
+
+  @Autowired
+  private FridgeRepository fridgeRepository;
+
+  @Autowired
+  private UserRepository userRepository;
+
+//  @Autowired
+//  private FridgeService fridge;
+
+  @Test
+  void testUpdateFoodNewFood() throws Exception {
+    FridgeService fridge = new FridgeService(fridgeRepository, userRepository);
+
+    fridge.updateFood(new UpdateQuantity(5L, 7L), 1234L, "chocolate");
+    Optional<Food> food = this.fridgeRepository.findUsersFood("chocolate", 1234L);
+    assertThat(food.isPresent()).isTrue();
+    assertThat(food.get().getUserId()).isEqualTo(1234L);
+    assertThat(food.get().getFoodName()).isEqualTo("chocolate");
+    assertThat(food.get().getFoodQuantity()).isEqualTo(5L);
+    assertThat(food.get().getCoreQuantity()).isEqualTo(7L);
+  }
+
+  @Test
+  void testUpdateFoodUpdateFoodQuantity() throws Exception {
+    FridgeService fridge = new FridgeService(fridgeRepository, userRepository);
+
+    fridge.updateFood(new UpdateQuantity(5L, 7L), 1234L, "chocolate");
+    fridge.updateFood(new UpdateQuantity(12L, 8L), 1234L, "chocolate");
+    fridge.updateFood(new UpdateQuantity(13L, 9L), 1234L, "chocolate");
+
+    Optional<Food> food = this.fridgeRepository.findUsersFood("chocolate", 1234L);
+    assertThat(food.isPresent()).isTrue();
+    assertThat(food.get().getUserId()).isEqualTo(1234L);
+    assertThat(food.get().getFoodName()).isEqualTo("chocolate");
+    assertThat(food.get().getCoreQuantity()).isEqualTo(9L);
+
+    assertThat(food.get().getFoodQuantity()).isEqualTo(30L);
+  }
+
+}
