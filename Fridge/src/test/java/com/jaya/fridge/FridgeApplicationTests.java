0f431a90369b688ec3f1495e919f56d0293e3ab0
Yael Kelmer <yrk2107@barnard.edu>
2021-11-15 23:48:48 -0500

    updated FridgeApplicationsTests

--- Fridge/src/test/java/com/jaya/fridge/FridgeApplicationTests.java
+++ Fridge/src/test/java/com/jaya/fridge/FridgeApplicationTests.java
@@ -31,63 +31,7 @@ import java.util.stream.Stream;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class FridgeApplicationTests {
-
-    @Autowired 
-	private FridgeService fridgeService;
-
-    @MockBean
-    private FridgeRepository fridgeRepository;
-
-    @Test
-    public void getFridgeAll(){
-        when(fridgeRepository.findAll()).thenReturn(Stream
-        .of(new Food(1L, "carrot", 2L, 3L), new Food(43L, "cake", 3L))
-        .collect(Collectors.toList()));
-        assertEquals(2, fridgeService.getFridgeAll().size());
-    }
-	
-    @Test
-    public void missingCoreTest(){
-        Long userID = 1L;
-        Food f = new Food(1L, "carrot", 2L, 3L);
-        List<Food> l = new ArrayList<>();
-        l.add(f);
-        when(fridgeRepository.findUsersFridge(userID)).thenReturn(Stream
-        .of(f, new Food(43L, "cake", 3L))
-        .collect(Collectors.toList()));
-        assertEquals(l, fridgeService.missingCore(userID));
-
-    }
-
-    @Test
-    public void updateAddNewFoodTest(){
-        Long userID = 1L;
-        Food f = new Food(userID, "carrot", 2L, 3L);
-        UpdateQuantity uq = new UpdateQuantity(f.getFoodQuantity(),f.getCoreQuantity());
-        when(fridgeRepository.save(f)).thenReturn(f);
-        assertEquals(f.toString(), fridgeService.updateFood(uq, userID, f.getFoodName()).toString());
-    }
-
-    // @Test
-    // public void updateFoodTest(){
-    //     fridgeService.updateFood(new UpdateQuantity(5L, 7L), 1234L, "chocolate");
-
-    //     Optional<Food> food = this.fridgeRepository.findUsersFood("chocolate", 1234L);
-    //     assertEquals(food.toString(), fridgeService.updateFood(uq, userID, f.getFoodName()).toString());
-    //     // Long userID = 1L;
-    //     // Food f = new Food(userID, "carrot", 2L, 3L);
-    //     // UpdateQuantity uq = new UpdateQuantity(f.getFoodQuantity(),f.getCoreQuantity());
-    //     // when(fridgeRepository.findUsersFridge(userID)).thenReturn(Stream
-    //     // .of(new Food(userID, "carrot", 4L, 53L), new Food(43L, "cake", 3L))
-    //     // .collect(Collectors.toList()));
-    //     // doNothing().when(fridgeService.updateFood(uq, userID, f.getFoodName()));
-    //     // fridgeService.updateFood(uq, userID, f.getFoodName());
-    //     // assertEquals(f.toString(), fridgeService.updateFood(uq, userID, f.getFoodName()).toString());
-    // }
-
-        
+    
 }
 
